5c2b898f26dd8a7cf2bcb491799bf20ecd8df7c5
Frank Harkins <frankharkins@hotmail.co.uk>
2023-02-23 19:31:52 +0000

    Update Vale rules

--- test/docs/vale.sh
+++ test/docs/vale.sh
@@ -4,9 +4,9 @@ cd ./docs
 
 vale .
 
-notebooks=$(find . -name "*.ipynb")
+notebooks=$(find . -name "*.ipynb" -not -name "*checkpoint*")
 
 for notebook in ${notebooks}
 do
-  python -m nbqa vale ${notebook} --nbqa-shell --nbqa-md || exit 1
+  python -m nbqa vale ${notebook} --nbqa-shell --nbqa-md
 done
